a4c234b51f1338d48cfcaec49f5a229c1953e90d
vczb <viniciuszucatti@gmail.com>
2020-10-03 19:09:05 -0300

    first commit

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+const { User } = require('../../src/app/models');
+const truncate = require('../utils/truncate');
+
+describe('Authentication', ()=>{
+    beforeEach(async () => {
+        await truncate();
+    })
+    it('should authenticate with valid credentials',  async ()=>{
+        const user = await User.create({
+            name: 'Vinicius',
+            email: 'viniciuszucatti@gmail.com',
+            password_hash: '123456',
+        })
+
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: '123456'
+            })
+        
+        expect(response.status).toBe(200);
+    });
+
+
+})
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const { route } = require('./app');
+
+const routes = require('express').Router();
+
+const { User } = require('./app/models');
+
+const SessionController = require('./app/controllers/SessionController');
+
+routes.post('/sessions', SessionController.store);
+
+module.exports = routes;
