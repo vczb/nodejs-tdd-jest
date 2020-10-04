b71444430db840e543c05a46a8309c5c03b5a52c
vczb <viniciuszucatti@gmail.com>
2020-10-03 21:47:32 -0300

    factories

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ const { route } = require('./app');
 
 const routes = require('express').Router();
 
-const { User } = require('./app/models');
-
 const SessionController = require('./app/controllers/SessionController');
 
 routes.post('/sessions', SessionController.store);
