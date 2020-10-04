da2dfb984c25042e0cf2d0ce14c23238781b093c
vczb <viniciuszucatti@gmail.com>
2020-10-03 22:12:41 -0300

    token

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -59,4 +59,31 @@ describe('Authentication', ()=>{
         
     });
 
+    it('should be able to access private routes when authenticated', async () =>{
+        const user = await factory.create('User', {
+            password: '123123'
+        })
+
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer ${user.generateToken()}`);
+        
+        expect(response.status).toBe(200);        
+    })
+
+    it('should not be able to access private routes without jwt token', async ()=>{
+        const response = await request(app)
+            .get('/dashboard');
+        
+        expect(response.status).toBe(401);        
+    })
+
+    it('should not be able to access private routes with invalid jwt token', async () => {
+        const response = await request(app)
+        .get('/dashboard')
+        .set('Authorization', `Bearer 123123`);   
+        
+        expect(response.status).toBe(401);        
+    })
+
 })
