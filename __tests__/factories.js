2f07a7a25ae7e6dbbee965cb4bbec6e3ee8db953
vczb <viniciuszucatti@gmail.com>
2020-10-03 21:52:13 -0300

    faker

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -1,10 +1,11 @@
-const {factory} = require('factory-girl');
+const faker = require('faker');
+const { factory } = require('factory-girl');
 const { User } = require('../src/app/models');
 
 factory.define('User', User, {
-    name: 'Vinicius',
-    email: 'viniciuszucatti@gmail.com',
-    password: '123456'
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
 })
 
 module.exports = factory;
